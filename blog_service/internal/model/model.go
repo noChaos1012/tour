6ddd6ed105f148c19c381511ec7e5a5469739d07
noChaos1012 <waschild@163.com>
2020-08-23 17:20:05 +0800

    init common gorm callback

--- blog_service/internal/model/model.go
+++ blog_service/internal/model/model.go
@@ -3,9 +3,10 @@ package model
 import (
 	"fmt"
 	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"github.com/noChaos1012/tour/blog_service/global"
 	"github.com/noChaos1012/tour/blog_service/pkg/setting"
-	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"time"
 )
 
 type Model struct {
@@ -35,7 +36,81 @@ func NewDBEngine(dbSetting *setting.DatabaseSettingS) (*gorm.DB, error) {
 		db.LogMode(true)
 	}
 	db.SingularTable(true)
+	//注册回调
+	db.Callback().Create().Replace("gorm:update_time_stamp", updateTimeStampForCreateCallback)
+	db.Callback().Update().Replace("gorm:update_time_stamp", updateTimeStampForUpdateCallback)
+	db.Callback().Delete().Replace("gorm:delete", deleteCallback)
+
 	db.DB().SetMaxIdleConns(dbSetting.MaxIdleConns)
 	db.DB().SetMaxOpenConns(dbSetting.MaxOpenConns)
 	return db, nil
+}
+
+//配置回调函数
+//新增行为的回调
+func updateTimeStampForCreateCallback(scope *gorm.Scope) {
+	if !scope.HasError() {
+		nowTime := time.Now().Unix()
+		//判断是否包含所需字段
+		if createTimeField, ok := scope.FieldByName("CreatedOn"); ok {
+			//如果是空则配置值
+			if createTimeField.IsBlank {
+				_ = createTimeField.Set(nowTime)
+			}
+		}
+
+		if modifyTimeField, ok := scope.FieldByName("ModifiedOn"); ok {
+			if modifyTimeField.IsBlank {
+				_ = modifyTimeField.Set(nowTime)
+			}
+		}
+	}
+}
+
+//更新行为的回调
+func updateTimeStampForUpdateCallback(scope *gorm.Scope) {
+	//获取当前设置标识的gorm:update_column字段属性
+	//若没有，则在更新回调中设置默认字段ModifiedOn为当前时间戳
+	if _, ok := scope.Get("gorm:update_column"); !ok {
+		_ = scope.SetColumn("ModifiedOn", time.Now().Unix())
+	}
+}
+
+//删除行为的回调
+func deleteCallback(scope *gorm.Scope) {
+	if !scope.HasError() {
+		var extraOption string
+		//获取删除标识 gorm:delete_option
+		if str, ok := scope.Get("gorm:delete_option"); ok {
+			extraOption = fmt.Sprint(str)
+		}
+
+		deleteOnField, hasDeletedOnField := scope.FieldByName("DeleteOn")
+		isDelField, hasIsDelField := scope.FieldByName("IsDel")
+		//进行软删除
+		if !scope.Search.Unscoped && hasDeletedOnField && hasIsDelField {
+			nowTime := time.Now().Unix()
+			scope.Raw(fmt.Sprintf("UPDATE %v SET %v=%v,%v=%v%v%v",
+				scope.QuotedTableName(),
+				scope.Quote(deleteOnField.DBName),
+				scope.AddToVars(nowTime),
+				scope.Quote(isDelField.DBName),
+				scope.AddToVars(1),
+				addExtraSpaceIfExist(scope.CombinedConditionSql()),
+				addExtraSpaceIfExist(extraOption), )).Exec()
+		} else {
+			scope.Raw(fmt.Sprintf("DELETE FROM %v%v%v",
+				scope.QuotedTableName(),
+				//scope.CombinedConditionSql 条件sql拼装
+				addExtraSpaceIfExist(scope.CombinedConditionSql()),
+				addExtraSpaceIfExist(extraOption))).Exec()
+		}
+	}
+}
+
+func addExtraSpaceIfExist(str string) string {
+	if str != "" {
+		return " " + str
+	}
+	return ""
 }
--- blog_service/internal/model/tag.go
+++ blog_service/internal/model/tag.go
@@ -1,6 +1,9 @@
 package model
 
-import "github.com/noChaos1012/tour/blog_service/pkg/app"
+import (
+	"github.com/jinzhu/gorm"
+	"github.com/noChaos1012/tour/blog_service/pkg/app"
+)
 
 type TagSwagger struct {
 	List  []*Tag
@@ -15,4 +18,45 @@ type Tag struct {
 
 func (t Tag) TableName() string {
 	return "blog_tag"
+}
+
+func (t Tag) Count(db *gorm.DB) (int, error) {
+	var count int
+	if t.Name != "" {
+		db = db.Where("name = ?", t.Name)
+	}
+	db = db.Where("state = ?", t.State)
+	//Model指定DB操作的模型实例，默认解析该结构体的名字为表名，格式为大写驼峰转小写下划线驼峰
+	if err := db.Model(&t).Where("is_del = ?", 0).Count(&count).Error; err != nil {
+		return 0, err
+	}
+	return count, nil
+}
+
+func (t Tag) List(db *gorm.DB, pageOffset, pageSize int) ([]*Tag, error) {
+	var tags []*Tag
+	var err error
+	if pageOffset >= 0 && pageSize > 0 {
+		db = db.Offset(pageOffset).Limit(pageSize)
+	}
+	if t.Name != "" {
+		db = db.Where("name = ?", t.Name)
+	}
+	db = db.Where("state = ?", t.State)
+	if err = db.Where("is_del = ?", 0).Find(&tags).Error; err != nil {
+		return nil, err
+	}
+	return tags, nil
+}
+
+func (t Tag) Create(db *gorm.DB) error {
+	return db.Create(&t).Error
+}
+
+func (t Tag) Update(db *gorm.DB, values interface{}) error {
+	return db.Model(&t).Where("id = ? AND is_del = ?", t.ID, 0).Update(values).Error
+}
+
+func (t Tag) Delete(db *gorm.DB) error {
+	return db.Where("id = ? AND is_del = ?", t.ID, 0).Delete(&t).Error
 }
